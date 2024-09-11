916ea498c121e57a226712c35256e97f1f7ff302
konradhorber <konrad.horber@gmail.com>
2024-09-11 12:35:19 +0200

    Implement working screen with event listeners

--- script.js
+++ script.js
@@ -4,9 +4,37 @@ let number1;
 let operator;
 let number2;
 
+// event listener for button clicks
+let lastClick;
+let lastInputNumber = "";
+let secondLastInputNumber;
+let lastOperator;
+const buttons = document.querySelectorAll(".button");
+const screen = document.querySelector(".calculator-screen")
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        lastClick = button.innerHTML;
+        if(isNumeric(lastClick)) {
+            lastInputNumber += lastClick;
+            screen.innerHTML=lastInputNumber;
+        } else {
+            lastOperator = lastClick;
+            secondLastInputNumber = Number(lastInputNumber);
+            lastInputNumber = "";
+        }
+    })
+})
 
+// function to check if number
+function isNumeric(str) {
+    if (typeof str != "string") return false
+    return !isNaN(str) &&
+           !isNaN(parseFloat(str))
+}
+
+// operator to feed button clicks to calculation operation functions
 function operate(operator, number1, number2) {
-    return [operator](number1, number2);
+    return operator(number1, number2);
 }
 
 // calculator operation functions
